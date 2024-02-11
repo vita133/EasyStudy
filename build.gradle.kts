0c99c38847825f6291d3e77ec7ce9f9e11f581ff
vita133 <vitandrukhiv@gmail.com>
2024-02-11 20:48:22 +0200

    add configurations for app distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id ("androidx.navigation.safeargs") version "2.7.4" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
